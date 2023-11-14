df8a2284b0de91b1bc54bfa0f64c14cae986bd57
wilhelmagren <wilhelmagren98@gmail.com>
2023-11-14 23:40:08 +0100

    feat(rust): test extern C read arrow array chunks

--- alloy-rs/src/chunks.rs
+++ alloy-rs/src/chunks.rs
@@ -82,4 +82,16 @@ pub unsafe fn read_data_from_array_ptr(
 
 #[cfg(test)]
 pub mod tests {
+
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_alloy_read_array_chunks_all_empty() {
+        let _n_chunks_read: c_uint = alloy_read_array_chunks(
+            &ffi::ArrowSchema::empty(),
+            &ffi::ArrowArray::empty(),
+            1 as usize,
+        );
+    }
 }
